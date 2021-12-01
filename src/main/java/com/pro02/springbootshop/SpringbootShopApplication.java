e512fd09ff3084d57083d8747e856cc5f91c15a2
yjeom <eomyj0920@gmail.com>
2021-12-01 14:41:16 +0900

    start

---
+++ src/main/java/com/pro02/springbootshop/SpringbootShopApplication.java
@@ -0,0 +1,13 @@
+package com.pro02.springbootshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootShopApplication.class, args);
+	}
+
+}
