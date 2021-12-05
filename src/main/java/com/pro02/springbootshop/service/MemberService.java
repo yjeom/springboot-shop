19857bb667e66bc0f3524baba8e3e729bb2de825
yjeom <eomyj0920@gmail.com>
2021-12-05 16:48:37 +0900

    login/logout

--- src/main/java/com/pro02/springbootshop/service/MemberService.java
+++ src/main/java/com/pro02/springbootshop/service/MemberService.java
@@ -3,13 +3,17 @@ package com.pro02.springbootshop.service;
 import com.pro02.springbootshop.entity.Member;
 import com.pro02.springbootshop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -23,5 +27,19 @@ public class MemberService {
         if(findMember != null){
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member=memberRepository.findByEmail(email);
+
+        if(member ==null){
+            throw new UsernameNotFoundException(email);
+        }
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
