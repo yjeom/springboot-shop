9557c90d815e4e8fd3301b6d4be1ec546bd16793
yjeom <eomyj0920@gmail.com>
2021-12-14 15:21:42 +0900

    order history list

--- src/main/java/com/pro02/springbootshop/repository/OrderRepository.java
+++ src/main/java/com/pro02/springbootshop/repository/OrderRepository.java
@@ -1,7 +1,21 @@
 package com.pro02.springbootshop.repository;
 
 import com.pro02.springbootshop.entity.Order;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+    @Query("SELECT o FROM Order o "+
+            "WHERE o.member.email = :email "+
+            "ORDER BY o.orderDate DESC")
+    List<Order> findOrders(@Param("email")String email, Pageable pageable);
+
+    @Query("SELECT COUNT(o) FROM Order o "+
+            "WHERE o.member.email = :email")
+    Long ocuntOrder(@Param("email")String email);
 }
--- src/main/java/com/pro02/springbootshop/service/OrderService.java
+++ src/main/java/com/pro02/springbootshop/service/OrderService.java
@@ -1,14 +1,18 @@
 package com.pro02.springbootshop.service;
 
 import com.pro02.springbootshop.dto.OrderDto;
-import com.pro02.springbootshop.entity.Item;
-import com.pro02.springbootshop.entity.Member;
-import com.pro02.springbootshop.entity.Order;
-import com.pro02.springbootshop.entity.OrderItem;
+import com.pro02.springbootshop.dto.OrderHistDto;
+import com.pro02.springbootshop.dto.OrderItemDto;
+import com.pro02.springbootshop.entity.*;
+import com.pro02.springbootshop.repository.ItemImgRepository;
 import com.pro02.springbootshop.repository.ItemRepository;
 import com.pro02.springbootshop.repository.MemberRepository;
 import com.pro02.springbootshop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,6 +28,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto,String email){
         Item item=itemRepository.findById(orderDto.getItemId())
@@ -37,5 +42,26 @@ public class OrderService {
         Order order=Order.createOrder(member,orderItemList);
         orderRepository.save(order);
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable){
+        List<Order> orders=orderRepository.findOrders(email,pageable);
+        Long totalCount=orderRepository.ocuntOrder(email);
+
+        List<OrderHistDto> orderHistDtos=new ArrayList<>();
+
+        for(Order order:orders){
+            OrderHistDto orderHistDto=new OrderHistDto(order);
+            List<OrderItem> orderItems=order.getOrderItems();
+            for(OrderItem orderItem:orderItems){
+                ItemImg itemImg=itemImgRepository
+                        .findByItemIdAndRepImgYn(orderItem.getItem().getId(),"Y");
+                OrderItemDto orderItemDto=new OrderItemDto(orderItem,itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+            orderHistDtos.add(orderHistDto);
+        }
+        return new PageImpl<OrderHistDto>(orderHistDtos,pageable,totalCount);
     }
 }
